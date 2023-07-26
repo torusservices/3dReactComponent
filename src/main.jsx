7fc260ad42e29b11b0bbabb3cc4d499eaef4f441
JB <blazinskasjonas@gmail.com>
2023-07-26 22:30:34 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { Canvas } from "@react-three/fiber"
+import { useGLTF, Stage, OrbitControls, Edges } from "@react-three/drei"
+import "./styles.css"
+import r2wc from "@r2wc/react-to-web-component"
+
+function Model() {
+  const { nodes } = useGLTF("/headless.glb")
+  return (
+    <group dispose={null}>
+      <mesh geometry={nodes.Cube.geometry}>
+        <meshStandardMaterial transparent />
+        <Edges />
+      </mesh>
+    </group>
+  )
+}
+
+function MyCanvas() {
+  return (
+    <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 3], fov: 50 }}>
+      <Stage contactShadow={{ resolution: 1024, scale: 10 }}>
+        <Model />
+      </Stage>
+      <OrbitControls makeDefault dampingFactor={0.3} />
+    </Canvas>
+  )
+}
+
+const HelloWC = r2wc(MyCanvas, {
+  props: { name: "string" }
+})
+
+customElements.define("hello-wc", HelloWC)
